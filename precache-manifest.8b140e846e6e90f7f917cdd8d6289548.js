e04fc847a8a704ce59d01d430046c7dfb47ac5dc
supatsaraporn <supatsaraporn.wo.61@ubu.ac.th>
2020-09-02 11:01:42 +0700

    Updates

---
+++ precache-manifest.8b140e846e6e90f7f917cdd8d6289548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0698dab111ef7165d18fb92ba0f8df9",
+    "url": "/MY-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "812b2fb61171be123219",
+    "url": "/MY-tic-tac-toe/static/css/main.f4b9b883.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/MY-tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MY-tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812b2fb61171be123219",
+    "url": "/MY-tic-tac-toe/static/js/main.94a86539.chunk.js"
+  },
+  {
+    "revision": "c6619486a52dd73830ca",
+    "url": "/MY-tic-tac-toe/static/js/runtime-main.29457373.js"
+  }
+]);
